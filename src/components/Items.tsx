ec00cd64b5f2ceacd16da5011e7f53908899ab5f
div0021 <div2wk@gmail.com>
2023-06-15 18:44:00 +0530

    Add not-found

--- src/components/Items.tsx
+++ src/components/Items.tsx
@@ -10,27 +10,25 @@ const Items = () => {
   const [width, setWidth] = useState<number>(window.innerWidth);
 
   const data1 = data.filter((el) => {
+    let guest = 0;
 
-    let guest =0;
-
-    if(search.guests.toString() === 'Add guests'){
-      guest=0;
-    }else{
+    if (search.guests.toString() === "Add guests") {
+      guest = 0;
+    } else {
       guest = Number(search.guests.toString().split(" ")[0]);
     }
 
-    if(search.location==='Add location'){
-  
-      return el.maxGuests>= guest;
+    if (search.location === "Add location") {
+      return el.maxGuests >= guest;
     }
-  
+
     return (
       el.city === search.location.split(", ")[0] &&
       el.country === search.location.split(", ")[1] &&
       el.maxGuests >= guest
     );
   });
-// console.log(data1)
+  // console.log(data1)
   const data11 = [];
   const data12 = [];
   const data13 = [];
@@ -81,14 +79,7 @@ const Items = () => {
   }, [search]);
 
   window.addEventListener("resize", () => {
-    if (
-      window.innerWidth === 769 ||
-      window.innerWidth === 766 ||
-      window.innerWidth === 1278 ||
-      innerWidth === 1280
-    ) {
-      setWidth(window.innerWidth);
-    }
+    setWidth(window.innerWidth);
   });
 
   return (
@@ -97,6 +88,20 @@ const Items = () => {
         <h1 className="font-bold text-lg">Stays in Finland</h1>
         <span className="text-sm">{stays}</span>
       </div>
+
+      {/* Not Available Section */}
+      {data1.length === 0 && (
+        <div
+          className="bg-cover bg-no-repeat micro:w-[94dvw] esm:w-[94dvw] vsm:w-[72dvw] md:w-[95dvw] big:w-[88.5dvw] lg:w-[80dvw] h-96 rounded-xl flex justify-center items-center flex-col space-y-3"
+          style={{
+            background:
+              "url(https://images.unsplash.com/photo-1516455590571-18256e5bb9ff?ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80)",
+          }}
+        >
+          <p className="text-4xl font-bold">😔 Sorry</p>
+          <p className="text-4xl font-bold">Not Available</p>
+        </div>
+      )}
 
       <div className="md:grid grid-cols-2 gap-5 xl:grid-cols-3">
         <div className="xl:space-y-2">
@@ -138,17 +143,6 @@ const Items = () => {
             />
           ))}
         </div>
-        {/* {data1.map((el, index) => (
-        <ListItem
-          key={index}
-          title={el.title}
-          superHost={el.superHost}
-          photo={el.photo}
-          beds={el.beds}
-          rating={el.rating}
-          type={el.type}
-        />
-      ))} */}
       </div>
     </div>
   );
